b13a50dd795412f51052713d9d50275f52d7e610
Mark Fabon <mark.fabon29@gmail.com>
2024-10-06 22:57:44 +0800

    Fix line order and add inplace to rename

--- ETL_pandas_4_table_solution.py
+++ ETL_pandas_4_table_solution.py
@@ -17,17 +17,18 @@ def main():
         sales_table  = pd.read_sql_query(sales_query,conn)
         customer_table  = pd.read_sql_query(customers_query,conn)   
 
-        orders_items = order_table.merge(item_table, on="item_id")
-        sales_customers = sales_table.merge(customer_table, on="customer_id")
-
-    #Pandas Solution
+#Pandas Solution
+    orders_items = order_table.merge(item_table, on="item_id")
+    sales_customers = sales_table.merge(customer_table, on="customer_id")
     ordered_sales = pd.merge(orders_items, sales_customers, on="sales_id")
+
+    ordered_sales = ordered_sales[["customer_id", "age", "item_name", "quantity"]].dropna() 
     ordered_sales = ordered_sales[(ordered_sales['age'] >= 18) & (ordered_sales['age'] <= 35) & (ordered_sales['quantity'] > 0)]
-    ordered_sales = ordered_sales[["customer_id", "age", "item_name", "quantity"]].dropna()
     ordered_sales['quantity'] = ordered_sales['quantity'].astype(int)
 
     total_item_quantity = ordered_sales.groupby(['customer_id','item_name','age'])['quantity'].sum().reset_index()
-    total_item_quantity.rename(columns={'customer_id':'Customer', 'item_name':'Item', 'age':'Age', 'quantity':'Quantity'})
+    total_item_quantity.rename(columns={'customer_id':'Customer', 'item_name':'Item', 'age':'Age', 'quantity':'Quantity'}, inplace=True)
+    
     total_item_quantity.to_csv('./total_quantites_of_item2.csv', sep=';', index=False)
         
 if __name__ == "__main__":
--- ETL_pandas_solution.py
+++ ETL_pandas_solution.py
@@ -18,14 +18,16 @@ def main():
         order_item_table  = pd.read_sql_query(order_item_query,conn)
         sales_customer_table  = pd.read_sql_query(sales_customer_query,conn)
 
-    #Pandas Solution
+#Pandas Solution
     ordered_sales = pd.merge(order_item_table, sales_customer_table, on="sales_id")
-    ordered_sales = ordered_sales[(ordered_sales['age'] >= 18) & (ordered_sales['age'] <= 35) & (ordered_sales['quantity'] > 0)]
+
     ordered_sales = ordered_sales[["customer_id", "age", "item_name", "quantity"]].dropna()
+    ordered_sales = ordered_sales[(ordered_sales['age'] >= 18) & (ordered_sales['age'] <= 35) & (ordered_sales['quantity'] > 0)]
     ordered_sales['quantity'] = ordered_sales['quantity'].astype(int)
 
     total_item_quantity = ordered_sales.groupby(['customer_id','item_name','age'])['quantity'].sum().reset_index()
-    total_item_quantity.rename(columns={'customer_id':'Customer', 'item_name':'Item', 'age':'Age', 'quantity':'Quantity'})
+    total_item_quantity.rename(columns={'customer_id':'Customer', 'item_name':'Item', 'age':'Age', 'quantity':'Quantity'}, inplace=True)
+    
     total_item_quantity.to_csv('./total_quantites_of_item.csv', sep=';', index=False)
         
 if __name__ == "__main__":
